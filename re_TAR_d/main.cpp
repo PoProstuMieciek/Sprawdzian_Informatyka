ed6886567e21d119bd2d69d2d504dcf4a530444f
Maciej Opaliński <poprostumieciek@gmail.com>
2022-06-15 22:38:09 +0200

    refactor(re_TAR_d): Optimize ram usage by working on streams
    !!! BUG IN DECOMPRESSION !!!

--- re_TAR_d/main.cpp
+++ re_TAR_d/main.cpp
@@ -2,13 +2,13 @@
 #include <cmath>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <chrono>
+#include <assert.h>
 using namespace std;
 using namespace std::chrono;
 
 // int_to_binary_string(5, 8) = "00000101"
-string int_to_binary_string(int number, int total_length)
+string int_to_binary_string(unsigned int number, int total_length)
 {
     string binary_string = "";
 
@@ -31,7 +31,7 @@ string int_to_binary_string(int number, int total_length)
 }
 
 // binary_string_to_int("101") = 5
-int binary_string_to_int(string binary_string)
+unsigned int binary_string_to_int(string binary_string)
 {
     int result = 0;
     for (int i = 0; i < binary_string.size(); i++)
@@ -42,116 +42,185 @@ int binary_string_to_int(string binary_string)
     return result;
 }
 
-// get unique characters in alphabetical order
-string generate_dictionary(const string &text)
+// read data from stream, save dictionary with all unique characters in alphabetical order, return size of stream
+unsigned long long generate_dictionary_from_stream(ifstream &stream, string &dictionary)
 {
-    bool tab[256] = {};
-    for (int i = 0; i < text.size(); i++)
+    unsigned long long bytes_read = 0;
+    bool byte_exists[256] = {};
+
+    // unsigned char = byte, byte range: (0, 255)
+    unsigned char current_byte;
+    while (stream.get((char &) current_byte))
     {
-        tab[(unsigned char) text[i]] = true;
+        bytes_read++;
+        byte_exists[current_byte] = true;
     }
 
-    string unique = "";
+    // loop over every existing byte
+    dictionary = "";
     for (int i = 0; i < 256; i++)
     {
-        if (tab[i]) unique += (char) i;
+        if (byte_exists[i]) dictionary += i;
     }
-    return unique;
+
+    return bytes_read;
 }
 
-string compress_text(const string &text)
+// read data from stream, return dictionary
+string restore_dictionary_from_stream(ifstream &stream)
 {
-    string dict = generate_dictionary(text);
+    unsigned char current_byte;
+
+    stream.get((char &) current_byte);
+
+    unsigned int dictionary_size = current_byte;
+
+    // this is an ugly edge case, byte has range (0, 255), but dictionary can have size of 256, so 256 is saved as 0
+    // however we cannot have a dictionary of size 0, that makes no sense
+    if (dictionary_size == 0) dictionary_size = 256;
+
+    string dictionary;
+
+    for (int i = 0; i < dictionary_size; i++)
+    {
+        stream.get((char &) current_byte);
+
+        dictionary += current_byte;
+    }
+
+    // just to catch any bugs
+    assert(dictionary.size() == dictionary_size);
+
+    return dictionary;
+}
+
+// gets dictionary, reads input_stream byte by byte, compresses data, outputs byte by byte to output_stream, returns size of compressed file in bytes
+unsigned long long compress_stream(const string &dictionary, ifstream &input_stream, unsigned long long input_stream_size, ofstream &output_stream)
+{
+    unsigned long long compressed_bytes = 0;
 
     // ceil(log2(x)) gives number of bits required to save number in binary format
-    int key_bit_size = max(1, (int) ceil(log2(dict.size())));
+    int key_bit_size = max(1, (int) ceil(log2(dictionary.size())));
 
-    // // print dictionary
-    // for (int i = 0; i < dict.size(); i++)
-    // {
-    //     cout << dict[i] << " --> " << int_to_binary_string(i, key_bit_size) << endl;
-    // }
-
-    int compressed_bits_size = key_bit_size * text.size();
+    // calculate total padding
+    int compressed_bits_size = key_bit_size * input_stream_size;
     int padding = (8 - ((compressed_bits_size + 3) % 8)) % 8;
 
-    string final_binary_text = int_to_binary_string(padding, 3);
+    // first byte of output is the size of dictionary, lets call it N
+    output_stream << (unsigned char) dictionary.size();
+    compressed_bytes++;
 
-    // for each char from original text
-    for (int i = 0; i < text.size(); i++)
+    // next N bytes are the dictionary
+    output_stream << dictionary;
+    compressed_bytes += dictionary.size();
+
+    // next we have padded data until the end of file
+    string buffer = int_to_binary_string(padding, 3);
+    buffer.reserve(16); // we dont need larger buffer
+
+    unsigned char current_byte;
+    while (input_stream.get((char &) current_byte))
     {
-        // find value in dictionary
-        for (int j = 0; j < dict.size(); j++)
+        // search for index in dictionary
+        for (int i = 0; i < dictionary.size(); i++)
         {
-            if (text[i] == dict[j])
+            if (current_byte == dictionary[i])
             {
-                final_binary_text += int_to_binary_string(j, key_bit_size);
+                buffer += int_to_binary_string(i, key_bit_size);
                 break;
             }
         }
+
+        // save all bytes if we have any
+        while (buffer.size() >= 8)
+        {
+            // get first 8 bits from buffer, and save to string
+            string byte_string_from_buffer = buffer.substr(0, 8);
+
+            // remove those first 8 bits
+            buffer.erase(0, 8);
+
+            // convert that string to integer
+            unsigned char byte_from_buffer = binary_string_to_int(byte_string_from_buffer);
+
+            // pipe to output
+            output_stream << byte_from_buffer;
+
+            compressed_bytes++;
+        }
     }
 
-    // apply padding
+    // apply padding at the end
     for (int i = 0; i < padding; i++)
     {
-        final_binary_text += '1';
+        buffer += '1';
     }
 
-    string compressed = "";
-
-    compressed += (char) dict.size();
-    compressed += dict;
-
-    string byte = "";
-    for (int i = 0; i < final_binary_text.size() / 8; i++)
+    // save all bytes if we have any, this is the same as above
+    while (buffer.size() >= 8)
     {
-        for (int j = 0; j < 8; j++)
-        {
-            byte += final_binary_text[i * 8 + j];
-        }
-        compressed += (char) binary_string_to_int(byte);
-        byte = "";
+        // get first 8 bits from buffer, and save to string
+        string byte_string_from_buffer = buffer.substr(0, 8);
+
+        // remove those first 8 bits
+        buffer.erase(0, 8);
+
+        // convert that string to integer
+        unsigned char byte_from_buffer = binary_string_to_int(byte_string_from_buffer);
+
+        // pipe to output
+        output_stream << byte_from_buffer;
+
+        compressed_bytes++;
     }
 
-    return compressed;
+    // buffer MUST be empty here. if it is not, then we have a bug!
+    assert(buffer.empty());
+
+    return compressed_bytes;
 }
 
-string decompress_text(const string &dict, const string &text_padded)
+void decompress_stream(const string &dictionary, ifstream &input_stream, unsigned long long input_stream_size, ofstream &output_stream)
 {
-    unsigned char first_byte = text_padded[0];
-    unsigned int padding = first_byte >> 5;
+    // ceil(log2(x)) gives number of bits required to save number in binary format
+    int key_bit_size = max(1, (int) ceil(log2(dictionary.size())));
 
-    string with_padding = "";
-    for (int i = 0; i < text_padded.size(); i++)
+    // calculate total padding
+    unsigned char current_byte;
+    input_stream.get((char &) current_byte);
+    int padding = current_byte >> 5;
+
+    string buffer = int_to_binary_string(current_byte, 8);
+    buffer.reserve(16); // we dont need larger buffer
+    buffer.erase(0, 3); // remove padding from beginning
+
+    unsigned long long bits_read = 8;
+    while (input_stream.get((char &) current_byte))
     {
-        with_padding += int_to_binary_string((unsigned char) text_padded[i], 8);
-    }
+        bits_read += 8;
+        buffer += int_to_binary_string(current_byte, 8);
 
-    string binary_string = "";
-    // skip 3 first characters (index 0, 1, 2) and padding at the end
-    for (int i = 3; i < with_padding.size() - padding; i++)
-    {
-        binary_string += with_padding[i];
-    }
-
-    int key_bit_size = max(1, (int) ceil(log2(dict.size())));
-
-    string decompressed_data = "";
-    string binary_cpr = "";
-    for (int i = 0; i < binary_string.size() / key_bit_size; i++)
-    {
-        for (int j = 0; j < key_bit_size; j++)
+        // save all bytes if we have any
+        while (buffer.size() >= key_bit_size && bits_read <= input_stream_size * 8 - padding)
         {
-            binary_cpr += binary_string[i * key_bit_size + j];
+            // let N = key_bit_size
+            // get first N bits from buffer, and save to string
+            string byte_string_from_buffer = buffer.substr(0, key_bit_size);
+
+            // remove those first N bits
+            buffer.erase(0, key_bit_size);
+
+            // convert that string to integer
+            unsigned char byte_from_buffer = binary_string_to_int(byte_string_from_buffer);
+
+            // pipe char from dictionary to output
+            output_stream << dictionary[byte_from_buffer];
         }
-
-        int index_in_dict = binary_string_to_int(binary_cpr);
-
-        decompressed_data += dict[index_in_dict];
-        binary_cpr = "";
     }
 
-    return decompressed_data;
+    // N = key_bit_size
+    // buffer MUST be of size N. if it is not, then we have a bug!
+    assert(buffer.size() == key_bit_size);
 }
 
 void compress_file(const string &input_filename, const string &output_filename, bool benchmark)
@@ -159,30 +228,33 @@ void compress_file(const string &input_filename, const string &output_filename, 
     // https://stackoverflow.com/a/22387757
     auto start = high_resolution_clock::now();
 
-    // https://stackoverflow.com/a/2602258
-    ifstream input_file(input_filename);
-    stringstream buffer;
-    buffer << input_file.rdbuf();
-    string file_content = buffer.str();
-    input_file.close();
+    // https://stackoverflow.com/a/7681612
+    // https://stackoverflow.com/a/22390788
+    ifstream input_stream(input_filename, ios::in | ios::binary);
+    ofstream output_stream(output_filename, ios::out | ios::binary);
 
-    string compressed = compress_text(file_content);
+    string dictionary;
+    unsigned long long raw_size_bytes = generate_dictionary_from_stream(input_stream, dictionary);
 
-    ofstream output_file(output_filename);
-    output_file << compressed;
-    output_file.close();
+    input_stream.clear();
+    input_stream.seekg(0, ios::beg);
+
+    unsigned long long compressed_size_bytes = compress_stream(dictionary, input_stream, raw_size_bytes, output_stream);
+
+    input_stream.close();
+    output_stream.close();
 
     auto stop = high_resolution_clock::now();
 
     if (benchmark)
     {
-        double percent_compressed = (1 - ((double) compressed.size() / (double) file_content.size())) * 100;
+        double percent_compressed = (1 - ((double) compressed_size_bytes / (double) raw_size_bytes)) * 100;
         duration<double, std::milli> time_ms = stop - start;
 
         cout << endl << "BENCHMARK" << endl;
         cout << "Compression time: " << time_ms.count() << " ms" << endl;
-        cout << "Original file size: " << file_content.size() << " B" << endl;
-        cout << "Compressed file size: " << compressed.size() << " B" << endl;
+        cout << "Original file size: " << raw_size_bytes << " B" << endl;
+        cout << "Compressed file size: " << compressed_size_bytes << " B" << endl;
         cout << "Compression ratio: " << percent_compressed << " %" << endl;
     }
 }
@@ -192,35 +264,29 @@ void decompress_file(const string &input_filename, const string &output_filename
     // https://stackoverflow.com/a/22387757
     auto start = high_resolution_clock::now();
 
-    // https://stackoverflow.com/a/2602258
-    // https://stackoverflow.com/a/17584871
-    ifstream input_file(input_filename, ios::binary);
-    stringstream buffer;
-    buffer << input_file.rdbuf();
-    string file_content = buffer.str();
-    input_file.close();
+    // https://stackoverflow.com/a/7681612
+    // https://stackoverflow.com/a/22390788
+    ifstream input_stream(input_filename, ios::in | ios::binary);
+    ofstream output_stream(output_filename, ios::out | ios::binary);
 
-    // recreate dictionary from file
-    int dict_size = (unsigned char) file_content[0];
-    if (dict_size == 0) dict_size = 256;
-    string dict = "";
-    for (int i = 0; i < dict_size; i++)
+    string dictionary = restore_dictionary_from_stream(input_stream);
+
+    // calculate file size
+    unsigned long long input_stream_size = 0;
+    char unused;
+    while (input_stream.get(unused))
     {
-        dict += file_content[1 + i];
+        input_stream_size++;
     }
 
-    // read compressed data with padding
-    string compressed_data = "";
-    for (int i = dict_size + 1; i < file_content.size(); i++)
-    {
-        compressed_data += file_content[i];
-    }
+    // back to place when we finished extracting the dictionary
+    input_stream.clear();
+    input_stream.seekg(dictionary.size() + 1);
 
-    string decompressed = decompress_text(dict, compressed_data);
+    decompress_stream(dictionary, input_stream, input_stream_size, output_stream);
 
-    ofstream output_file(output_filename);
-    output_file << decompressed;
-    output_file.close();
+    input_stream.close();
+    output_stream.close();
 
     auto stop = high_resolution_clock::now();
 
